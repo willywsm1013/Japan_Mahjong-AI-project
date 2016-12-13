d4b135c6378cd1628ba04264b7fe3d039d55e392
willywsm1013 <willywsm1013@gmail.com>
2016-12-14 02:11:13 +0800

    collect data

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from Table import Table
 from SimpleAgent import RandomAgent,OneStepAgent
-    
+from six.moves import cPickle    
 table = Table(True)
 record = [0]*4
-repeat = 100.0
+repeat = 10000.0
 data = []
+pickle_name = './enemyAnalysis_oneStep_1000_1213'
 for time in range(int(repeat)):
     table.newGame()
     for i in range(3):
-        table.addAgent(RandomAgent(i))
+        table.addAgent(OneStepAgent(i))
     table.addAgent(OneStepAgent(3))
     table.deal()
     winner = table.gameStart(True)
     if winner != None:
         record[winner]+=1
+enemyData = table.getThrowsAndCombination()
+print (enemyData)
+print ('data lenth = ',len(enemyData))
+f = open(pickle_name, 'wb')
+cPickle.dump(enemyData, f, protocol=cPickle.HIGHEST_PROTOCOL)
+f.close()
+print ('save data to ',pickle_name)
 print (record)
