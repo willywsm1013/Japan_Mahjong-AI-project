f1500224f3476d3de88eca6c0b0d5cb00595f1d4
willywsm1013 <willywsm1013@gmail.com>
2017-01-07 21:58:00 +0800

    parser

--- train_main.py
+++ train_main.py
@@ -7,23 +7,58 @@ import operator
 from six.moves import cPickle
 import matplotlib.pyplot as plt
 import time as timer
+import math
+import argparse
 
-Verbose = False
-UI = False
+
+
+##########################
+###   command Parser   ###
+##########################
+parser = argparse.ArgumentParser(description='')
+
+parser.add_argument('-train',"--train", action='store_true', help = "open training mode")
+parser.add_argument('-test',"--test", action='store_true', help = "open testing mode")
+parser.add_argument('-r',"--repeat", type=float,dest = "repeat", default = 1, help = "training or testing times[1]")
+parser.add_argument('-v','--verbose',action='store_true',help='print information')
+parser.add_argument('-ui','--UI',action='store_true',help='open user interface')
+parser.add_argument('-pw','--print_weight',action='store_true',help='print weight of learningAgent')
+parser.add_argument('-dlr','--decrease_learning_rate',action='store_true',help='decrease learning rate')
+parser.add_argument('-dep','--decrease_epsilon',action='store_true',help='decrease epsilon')
+
+
+args = parser.parse_args()
+
+Verbose = args.verbose
+UI = args.UI
+
+train = args.train
+test = args.test
+
+repeat = args.repeat
+print_weight = args.print_weight
+
+decreaseEpsilon = args.decrease_epsilon
+decreaseLearningRate = args.decrease_learning_rate
+
 plotQ = False
+rounds = 1000
 
-decreaseEpsilon = False
-train = False
+pickle = 'selfLearning（槓）（刻）（順）（明牌）（廣義孤張）（成刻對）（不成刻對）（雙牌）'
+
+#########################
+###   program start   ###
+#########################
+if print_weight:
+    agent = SelfLearningAgent(0,pickle_name=pickle)
+    sys.exit()
 
 table = Table(False)
 winRecord = [0]*4
 loseRecord = [0]*4
-repeat = 100000.0
-rounds = 1000
 data = []
 scores = [0]*4
 Round = 0
-pickle = 'selfLearning（槓）（刻）（順）（明牌）（孤張）（雀）'
 
 #pickle = 'selfLearningNoDecrease0.8'
 maxWin = 0
@@ -40,7 +75,7 @@ def linearDecay(value=None,decayRate=None,minimum=None,init = False):
         return max(v,linearDecay.minimun)
 
 def exponentialDecay(value = None):
-    decayRate = 0.95
+    decayRate = 0.9
     minimum = 1e-20
     v = value * decayRate
     return max(v,minimum)
@@ -72,26 +107,30 @@ def testing(table,playerNumber,rounds):
 
 winRate = []
 winCounts = []
+train_Q = []
 try :
     table.addAgent(RandomAgent(0))
     table.addAgent(RandomAgent(1))
     table.addAgent(RandomAgent(2))
     if train :
+        epsilon = 0.5
+        if decreaseEpsilon :
+            epsilon = 1
         table.addAgent(SelfLearningAgent(player_number = 3,
                                          discount = 0.8,
-                                         epsilon = 0.5,
+                                         epsilon = epsilon,
                                          alpha = 0.0001,
                                          mode = 'train',
                                          pickle_name=pickle,
                                          lr_decay_fn = exponentialDecay
                                         ))
         maxWin = testing(table,3,rounds)
-        if decreaseEpsilon:
-            table.agents[3].setEpsilon(1-maxWin/rounds)
-
         winRate.append(maxWin)
-    else :
+    elif test:
         table.addAgent(SelfLearningAgent(3,mode = 'test',pickle_name=pickle))
+    else :
+        print ('please use -train or -test flags')
+        sys.exit()
     
     maxWin = 0
     for time in range(int(repeat)):
@@ -124,6 +163,14 @@ try :
         if Verbose:
             print ('**************************')
         if train :
+            '''
+            qValue=table.agents[3].recordQ
+            train_Q.append(sum(qValue)/len(qValue)+1)
+            if plotQ and (time+1) % 1000 == 0:
+                plt.plot(train_Q)
+                plt.ylabel('Q value')
+                plt.show()
+            '''
             if (time+1) % 10000 == 0:
                 table.gameEnd()
                 testWin = testing(table,3,rounds)
@@ -137,19 +184,12 @@ try :
                     #input()
                 if decreaseEpsilon:
                     table.agents[3].setEpsilon(1-(time+1)/100000)
+               
             else :
                 table.gameEnd()
 
-            '''
-            if (time+1) % 1000 ==0 and decreaseEpsilon:
+            if (time+1) % 1000 ==0 and decreaseLearningRate:
                 table.agents[3].lrDecay()
-            '''
-        if plotQ :
-            qValue = table.agents[3].recordQ             
-            #plt.ion()
-            plt.plot(qValue)
-            plt.ylabel('Q value')
-            plt.show()
     print ('胡:', [winRecord[i]/Round for i in range(4)])
     print ('放槍:',[loseRecord[i]/Round for i in range(4)])
     print ('分數:',scores)
